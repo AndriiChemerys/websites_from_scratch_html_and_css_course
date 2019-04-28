16bb93d6df801cca7a8a7b7e619edf84e33ab930
AndriiChemerys <andriichemerys@gmail.com>
2019-04-28 17:32:02 +0200

    creating basic app structure

--- 4_complete_javascript_course/4-DOM-pig-game/starter/app.js
+++ 4_complete_javascript_course/4-DOM-pig-game/starter/app.js
@@ -18,10 +18,19 @@ activePlayer = 1;
 dice = Math.floor(Math.random()*6)+1;
 console.log(dice);
 
-document.querySelector('#current-'+activePlayer).textContent = dice;
-// document.querySelector('#current-'+activePlayer).innerHTML='<em>'+dice+'</em>'
-
-let x = document.querySelector('#score-0').textContent;
-console.log(x);
-
 document.querySelector('.dice').style.display='none';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    // 1. Random number
+    let dice = Math.floor(Math.random()*6)+1;
+    // 2. Display the result
+    let diceDOM = document.querySelector('.dice');
+    diceDOM.style.display='block';
+    diceDOM.src = 'dice-'+dice+ '.png';
+});
+
+// document.querySelector('#current-'+activePlayer).textContent = dice;
+// // document.querySelector('#current-'+activePlayer).innerHTML='<em>'+dice+'</em>'
+
+// let x = document.querySelector('#score-0').textContent;
+// console.log(x);
