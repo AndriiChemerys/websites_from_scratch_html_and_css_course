1f53176fde48ec67ef656ddede905d1f8fa9ff43
AndriiChemerys <andriichemerys@gmail.com>
2019-04-29 20:30:04 +0200

    if else challenge 2

--- 4_complete_javascript_course/4-DOM-pig-game/starter/app.js
+++ 4_complete_javascript_course/4-DOM-pig-game/starter/app.js
@@ -20,13 +20,29 @@ console.log(dice);
 
 document.querySelector('.dice').style.display='none';
 
+document.getElementById('score-0').textContent = "0";
+document.getElementById('score-1').textContent = "0";
+document.getElementById('current-0').textContent = "0";
+document.getElementById('current-1').textContent = "0";
+
+
 document.querySelector('.btn-roll').addEventListener('click', function() {
+
     // 1. Random number
     let dice = Math.floor(Math.random()*6)+1;
+
     // 2. Display the result
     let diceDOM = document.querySelector('.dice');
     diceDOM.style.display='block';
     diceDOM.src = 'dice-'+dice+ '.png';
+
+    // 3. Update the round score IF the rolled number was NOT a 1
+    if (dice !== 1) {
+        //add score
+        roundScore += dice;
+    } else {
+        //Next player
+    }
 });
 
 // document.querySelector('#current-'+activePlayer).textContent = dice;
