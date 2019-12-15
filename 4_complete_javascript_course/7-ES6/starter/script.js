64e9a76cce95f7caa3efaa2047bf2083a71d5fa8
AndriiChemerys <andriichemerys@gmail.com>
2019-12-15 20:18:07 +0100

    Blocks and IIFEs

--- 4_complete_javascript_course/7-ES6/starter/script.js
+++ 4_complete_javascript_course/7-ES6/starter/script.js
@@ -53,3 +53,27 @@
 
 // console.log(i);
 // */
+
+
+/////////////////////////////////
+// Lecture: Blocks and IIFEs
+
+/*
+// ES6
+{
+    const a = 1;
+    let b = 2;
+    var c = 3;
+}
+
+//console.log(a + b);
+console.log(c);
+
+
+// ES5
+(function() {
+    var c = 3;
+})();
+
+//console.log(c);
+*/
