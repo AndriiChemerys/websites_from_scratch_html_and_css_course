35decd80dbecdebaf6ed00ab9e0a678366a4cb4b
AndriiChemerys <andriichemerys@gmail.com>
2020-02-17 21:11:15 +0100

    arrays in ES6

--- 4_complete_javascript_course/7-ES6/starter/script.js
+++ 4_complete_javascript_course/7-ES6/starter/script.js
@@ -201,37 +201,82 @@ console.log(c);
 // new Person('Mike').myFriends6(friends);
 
 /////////////////////////////////
-// Lecture: Destructuring
+// // Lecture: Destructuring
+
+// //ES5
+// let john = ['John', 26];
+// // let name = john[0];
+// // let age = john[1];
+
+// //ES6 destructuring
+
+// const [name, age] = ['John', 26];
+// console.log(name);
+// console.log(age);
+
+// const obj = {
+//     firstName: 'John',
+//     lastName: 'Smith'
+// };
+
+// const {firstName, lastName} = obj;
+// console.log(firstName);
+// console.log(lastName);
+
+// const {firstName: a, lastName: b} = obj;
+// console.log(a);
+// console.log(b);
+
+// function calcAgeRetirement(year) {
+//     const age = new Date().getFullYear()-year;
+//     return [age, 65-age];
+// }
+
+// const [age, retiremnet] = calcAgeRetirement(1990);
+// console.log(age2);
+// console.log(retirement);
+
+////////////////////////////////
+//Lecture: Arrays
+
+const boxes=document.querySelectorAll('.box');
+console.log(boxes);
+
+// //ES5
+let boxesArr5=Array.prototype.slice.call(boxes);
+boxesArr5.forEach(function(cur){
+    cur.style.backgroundColor='dodgerblue'
+})
+
+// ES6
+const boxesArr6 = Array.from(boxes);
+Array.from(boxes).forEach(cur=>cur.style.backgroundColor='dodgerblue');
 
 //ES5
-let john = ['John', 26];
-// let name = john[0];
-// let age = john[1];
+// for(let i=0; i<boxesArr5.length; i++) {
+//     if(boxesArr5[i].className === 'boxblue') {
+//         continue;
+//     }
+//     boxesArr5[i].textContent = "I changed to blue";
+// }
 
-//ES6 destructuring
-
-const [name, age] = ['John', 26];
-console.log(name);
-console.log(age);
-
-const obj = {
-    firstName: 'John',
-    lastName: 'Smith'
-};
-
-const {firstName, lastName} = obj;
-console.log(firstName);
-console.log(lastName);
-
-const {firstName: a, lastName: b} = obj;
-console.log(a);
-console.log(b);
-
-function calcAgeRetirement(year) {
-    const age = new Date().getFullYear()-year;
-    return [age, 65-age];
+//ES6
+for(const cur of boxesArr6){
+    if(cur.className.includes("blue")){
+        continue;
+    }
+    cur.textContent="I changed to blue";
 }
 
-const [age, retiremnet] = calcAgeRetirement(1990);
-console.log(age2);
-console.log(retirement);
+//ES5
+let ages = [12, 17, 8, 21, 14, 11];
+let full = ages.map(function(cur) {
+    return cur >= 18;
+});
+console.log(full);
+console.log(full.indexOf(true));
+console.log(ages[full.indexOf(true)]);
+
+//ES6
+console.log(ages.findIndex(cur => cur >= 18));
+console.log(ages.find(cur => cur >= 18));
