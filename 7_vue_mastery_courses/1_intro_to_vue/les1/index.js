2148019c21ae7f33894b4ea8c95afafb32a36214
AndriiChemerys <andriichemerys@gmail.com>
2020-04-25 17:17:49 +0200

    Computed Properties

--- 7_vue_mastery_courses/1_intro_to_vue/les1/index.js
+++ 7_vue_mastery_courses/1_intro_to_vue/les1/index.js
@@ -1,37 +1,57 @@
 let app = new Vue({
     el: '#app',
     data: {
+        brand: 'Vue Mastery',
         product: 'Socks',
-        image: './img/socks_green.jpg',
+        selectedVariant: 0,
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
         inventory: 23,
-        inStock: false,
+        // inStock: true,
         onSale: true,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
-        variants: [
-            {
+        variants: [{
                 variantId: 2234,
                 variantColor: 'green',
-                variantImage: './img/socks_green.jpg'
+                variantImage: './img/socks_green.jpg',
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: "blue",
-                variantImage: './img/vmSocks-blue-onWhite.jpg'
+                variantImage: './img/vmSocks-blue-onWhite.jpg',
+                variantQuantity: 0
             }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0
     },
     methods: {
-        addToCart: function() {
+        addToCart() {
             this.cart += 1
         },
-        removeFromCart: function() {
+        removeFromCart() {
             this.cart -= 1
         },
-        updateProduct: function (variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index)
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' are on sale!'
+            }
+            return this.brand + ' ' + this.product + ' are not on sale'
         }
     }
 })
