511b1a911ef2815cfa2110e8d2d7ed0221ff9bea
AndriiChemerys <andriichemerys@gmail.com>
2020-04-25 12:46:13 +0200

    Conditional Rendering

--- 7_vue_mastery_courses/1_intro_to_vue/les1/index.js
+++ 7_vue_mastery_courses/1_intro_to_vue/les1/index.js
@@ -3,7 +3,8 @@ let app = new Vue({
     data: {
         product: 'Socks',
         image: './img/socks_green.jpg',
-        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
-
+        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+        inventory: 0,
+        onSale: false
     }
 })
