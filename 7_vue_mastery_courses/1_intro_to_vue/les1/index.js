22bdf58101388a7340cb3d1ad942406e62d40658
AndriiChemerys <andriichemerys@gmail.com>
2020-04-25 12:26:55 +0200

    first steps with vue

---
+++ 7_vue_mastery_courses/1_intro_to_vue/les1/index.js
@@ -0,0 +1,9 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: './img/socks_green.jpg',
+        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
+
+    }
+})
