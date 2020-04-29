4746f37899a8a48e4faf5c356fd55c0fe95f11db
AndriiChemerys <andriichemerys@gmail.com>
2020-04-29 15:04:23 +0200

    Vue Router Basics

---
+++ 7_vue_mastery_courses/2_real_world_vue/les1/real-world-vue/src/components/EventCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1 v-if="" @click="handler(arg, event)"></h1>
+</template>
+
+<script>
+export default {
+props: {
+    propName: {
+        type: Number,
+        default:
+    },
+},
+
+data() {
+    return {
+        key: value
+    }
+},
+}
+</script>
+
+<style></style>
