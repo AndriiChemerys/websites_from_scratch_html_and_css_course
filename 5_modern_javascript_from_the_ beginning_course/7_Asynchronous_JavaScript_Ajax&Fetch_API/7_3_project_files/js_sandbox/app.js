bc555c5297e7623410632e87e1ff8cda5e26837a
AndriiChemerys <andriichemerys@gmail.com>
2020-09-27 16:25:13 +0200

    Working With Ajax & JSON

--- 5_modern_javascript_from_the_ beginning_course/7_Asynchronous_JavaScript_Ajax&Fetch_API/7_3_project_files/js_sandbox/app.js
+++ 5_modern_javascript_from_the_ beginning_course/7_Asynchronous_JavaScript_Ajax&Fetch_API/7_3_project_files/js_sandbox/app.js
@@ -10,13 +10,13 @@ function loadData() {
   // console.log('READYSTATE', xhr.readyState);
 
   // Optional - Used for spinners/loaders
-  xhr.onprogress = function(){
+  xhr.onprogress = function () {
     console.log('READYSTATE', xhr.readyState);
   }
 
-  xhr.onload = function(){
+  xhr.onload = function () {
     console.log('READYSTATE', xhr.readyState);
-    if(this.status === 200) {
+    if (this.status === 200) {
       // console.log(this.responseText);
       document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`;
     }
@@ -29,20 +29,18 @@ function loadData() {
   //   }
   // }
 
-  xhr.onerror = function() {
+  xhr.onerror = function () {
     console.log('Request error...');
   }
 
   xhr.send();
 
-
-    // readyState Values
-    // 0: request not initialized 
-    // 1: server connection established
-    // 2: request received 
-    // 3: processing request 
-    // 4: request finished and response is ready
-
+  // readyState Values
+  // 0: request not initialized 
+  // 1: server connection established
+  // 2: request received 
+  // 3: processing request 
+  // 4: request finished and response is ready
 
   // HTTP Statuses
   // 200: "OK"
