72d5b1d35b7182ed2375f997cc3f1f4ca9ed42e7
AndriiChemerys <andriichemerys@gmail.com>
2020-10-13 20:13:58 +0200

    Callback Functions

--- 8_modern_javascript_from_novice_to_ninja_course/sec12-async_js/les1/script.js
+++ 8_modern_javascript_from_novice_to_ninja_course/sec12-async_js/les1/script.js
@@ -1,14 +1,26 @@
-const request = new XMLHttpRequest();
+const getTodos = (callback) => {
+    const request = new XMLHttpRequest();
 
-request.addEventListener('readystatechange', ()=> {
-    // console.log(request, request.readyState);
-    if(request.readyState===4 && request.status===200){
-        console.log(request, request.responseText);
-    } else if(request.readyState === 4) {
-        console.log('Could not fetch the data');
+    request.addEventListener('readystatechange', ()=> {
+        // console.log(request, request.readyState);
+        if(request.readyState===4 && request.status===200){
+            callback(undefined, request.responseText);
+        } else if(request.readyState === 4) {
+            callback('Could not fetch data', undefined);
+        }
+    })
+
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+    request.send();
+};
+
+getTodos((err, data)=> {
+    console.log('Callback fired');
+    // console.log(err, data);
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
     }
-})
-
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-request.send();
+});
 
