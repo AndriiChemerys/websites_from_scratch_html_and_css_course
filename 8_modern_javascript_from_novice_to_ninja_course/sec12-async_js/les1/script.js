504b205ecc9f3576b2965d86099f82409c547def
AndriiChemerys <andriichemerys@gmail.com>
2020-10-14 21:30:22 +0200

    JSON Data

--- 8_modern_javascript_from_novice_to_ninja_course/sec12-async_js/les1/script.js
+++ 8_modern_javascript_from_novice_to_ninja_course/sec12-async_js/les1/script.js
@@ -4,6 +4,7 @@ const getTodos = (callback) => {
     request.addEventListener('readystatechange', ()=> {
         // console.log(request, request.readyState);
         if(request.readyState===4 && request.status===200){
+            const data = JSON.parse(request.responseText);
             callback(undefined, request.responseText);
         } else if(request.readyState === 4) {
             callback('Could not fetch data', undefined);
