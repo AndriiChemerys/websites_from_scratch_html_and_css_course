795ea072a3f60e2b34f840245b861a9c75947050
AndriiChemerys <andriichemerys@gmail.com>
2020-10-17 14:44:01 +0200

    Promise Basics

--- 8_modern_javascript_from_novice_to_ninja_course/sec12-async_js/les1/script.js
+++ 8_modern_javascript_from_novice_to_ninja_course/sec12-async_js/les1/script.js
@@ -1,28 +1,58 @@
 const getTodos = (resource, callback) => {
-    const request = new XMLHttpRequest();
 
-    request.addEventListener('readystatechange', ()=> {
-        // console.log(request, request.readyState);
-        if(request.readyState===4 && request.status===200){
-            const data = JSON.parse(request.responseText);
-            callback(undefined, data);
-        } else if(request.readyState === 4) {
-            callback('Could not fetch data', undefined);
-        }
-    })
+    return new Promise((resolve, reject)=>{
+        const request = new XMLHttpRequest();
 
-    request.open('GET', resource);
-    // request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-    request.send();
+        request.addEventListener('readystatechange', ()=> {
+            // console.log(request, request.readyState);
+            if(request.readyState===4 && request.status===200){
+                const data = JSON.parse(request.responseText);
+                resolve(data);
+            } else if(request.readyState === 4) {
+                reject('error');
+            }
+        })
+    
+        request.open('GET', resource);
+        // request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+        request.send();
+    });
 };
 
-getTodos('todos/luigi.json', (err, data) => {
-    console.log(data);
-    getTodos('todos/mario.json', (err, data) => {
-        console.log(data);
-        getTodos('todos/shaun.json', (err, data) => {
-            console.log(data);
-        });
-    });
-});
+getTodos('todos/luigi.json').then(data=>{
+    console.log('Promise resolved:', data);
+}).catch((err)=>{
+    console.log('Promise rejected:', err)
+})
 
+// promise example 
+
+// getTodos('todos/luigi.json', (err, data) => {
+//     console.log(data);
+//     getTodos('todos/mario.json', (err, data) => {
+//         console.log(data);
+//         getTodos('todos/shaun.json', (err, data) => {
+//             console.log(data);
+//         });
+//     });
+// });
+
+const getSomething = () => {
+    return new Promise((resolve, reject)=>{
+        // fetch something
+        resolve('Some data');
+        reject('Some Error');
+    });
+};
+
+// getSomething().then((data)=>{
+//     console.log(data);
+// }, (err) => {
+//     console.log(err);
+// });
+
+getSomething().then(data => {
+    console.log(data);
+}).catch(err=>{
+    console.log(err);
+})
