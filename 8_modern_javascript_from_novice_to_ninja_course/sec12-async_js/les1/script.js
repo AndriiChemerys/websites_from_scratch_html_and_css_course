1a310209bc0942763ddd68367ed62e5b96d607cd
AndriiChemerys <andriichemerys@gmail.com>
2020-10-17 14:20:39 +0200

    JSON Data

--- 8_modern_javascript_from_novice_to_ninja_course/sec12-async_js/les1/script.js
+++ 8_modern_javascript_from_novice_to_ninja_course/sec12-async_js/les1/script.js
@@ -5,13 +5,14 @@ const getTodos = (callback) => {
         // console.log(request, request.readyState);
         if(request.readyState===4 && request.status===200){
             const data = JSON.parse(request.responseText);
-            callback(undefined, request.responseText);
+            callback(undefined, data);
         } else if(request.readyState === 4) {
             callback('Could not fetch data', undefined);
         }
     })
 
-    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+    request.open('GET', 'todos.json');
+    // request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
     request.send();
 };
 
