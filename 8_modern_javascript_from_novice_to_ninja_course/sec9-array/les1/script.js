f2955ad60db7c9544872e7406661120fd14615ce
AndriiChemerys <andriichemerys@gmail.com>
2020-11-04 20:29:27 +0100

    Chaining Array Methods

--- 8_modern_javascript_from_novice_to_ninja_course/sec9-array/les1/script.js
+++ 8_modern_javascript_from_novice_to_ninja_course/sec9-array/les1/script.js
@@ -3,6 +3,9 @@ const scores = [10, 5, 0, 40, 60, 10, 20, 70];
 
 const firstHighScore = scores.find((score) =>  score > 50);
 
+scores.sort((a,b) => a - b);
+console.log(scores);
+
 console.log(firstHighScore);
 
 // sorting objects
@@ -28,3 +31,27 @@ const players = [
 players.sort((a,b) => b.score - a.score);
 
 console.log(players);
+
+// Chaining Array Methods
+
+const products = [
+    {name: 'mario', price: 20},
+    {name: 'luigi', price: 10},
+    {name: 'chun-li', price: 50},
+    {name: 'yoshi', price: 30},
+    {name: 'shaun', price: 70},
+];
+
+// const filtered = products.filter(product => product.price > 20);
+
+// console.log(filtered);
+
+// const promos = filtered.map(product => {
+//     return `the ${product.name} is ${product.price / 2} pounds`
+// });
+
+const promos = products
+    .filter(product => product.price > 20)
+    .map(product => `the ${product.name} is ${product.price / 2} pounds`);
+
+console.log(promos);
