b253af4fa620e26f146661db165010edb1bfed9f
AndriiChemerys <andriichemerys@gmail.com>
2020-11-04 20:13:16 +0100

    Sort Method

--- 8_modern_javascript_from_novice_to_ninja_course/sec9-array/les1/script.js
+++ 8_modern_javascript_from_novice_to_ninja_course/sec9-array/les1/script.js
@@ -4,3 +4,27 @@ const scores = [10, 5, 0, 40, 60, 10, 20, 70];
 const firstHighScore = scores.find((score) =>  score > 50);
 
 console.log(firstHighScore);
+
+// sorting objects
+
+const players = [
+    {name: 'mario', score: 20},
+    {name: 'luigi', score: 10},
+    {name: 'chun-li', score: 50},
+    {name: 'yoshi', score: 30},
+    {name: 'shaun', score: 70},
+];
+
+// players.sort((a,b) => {
+//     if(a.score > b.score){
+//         return -1;
+//     } else if (b.score > a.score) {
+//         return 1;
+//     } else {
+//         return 0;
+//     }
+// });
+
+players.sort((a,b) => b.score - a.score);
+
+console.log(players);
