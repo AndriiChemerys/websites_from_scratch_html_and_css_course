00210bc0ab0391610a726393d94d4b3438a87410
AndriiChemerys <andriichemerys@gmail.com>
2020-11-05 16:25:39 +0100

    Other Ways to Query the DOM

--- 8_modern_javascript_from_novice_to_ninja_course/sec6-dom/les1/script.js
+++ 8_modern_javascript_from_novice_to_ninja_course/sec6-dom/les1/script.js
@@ -2,11 +2,30 @@
 
 // console.log(para);
 
-const paras = document.querySelectorAll('p');
-const errors = document.querySelectorAll('.error');
+// const paras = document.querySelectorAll('p');
+// const errors = document.querySelectorAll('.error');
 
-paras.forEach(para => {
-    console.log(para);
-});
+// paras.forEach(para => {
+//     console.log(para);
+// });
 
-console.log(errors);
+// console.log(errors);
+
+////////////////////////////////
+
+// get an element by ID
+
+// const title = document.getElementById('page-title');
+// console.log(title);
+
+// get elements by their class name
+
+// const errors = document.getElementsByClassName('error');
+// console.log(errors);
+// console.log(errors[0]);
+
+// get elements by their tag name
+
+const paras = document.getElementsByTagName('p');
+console.log(paras);
+console.log(paras[1]);
