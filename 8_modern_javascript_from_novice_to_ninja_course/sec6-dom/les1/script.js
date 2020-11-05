8492f44b758f394e7b54c914b64ec8b5411b073d
AndriiChemerys <andriichemerys@gmail.com>
2020-11-05 16:48:55 +0100

    Getting & Setting Attributes

--- 8_modern_javascript_from_novice_to_ninja_course/sec6-dom/les1/script.js
+++ 8_modern_javascript_from_novice_to_ninja_course/sec6-dom/les1/script.js
@@ -30,25 +30,36 @@
 // console.log(paras);
 // console.log(paras[1]);
 
-const para = document.querySelector('p');
+// const para = document.querySelector('p');
 
 // console.log(para.innerText);
 // para.innerText = 'Ninjas are awesome';
 
-const paras = document.querySelectorAll('p');
+// const paras = document.querySelectorAll('p');
 
-paras.forEach(para => {
-    console.log(para.innerText);
-    para.innerText += ' new text';
-})
+// paras.forEach(para => {
+//     console.log(para.innerText);
+//     para.innerText += ' new text';
+// })
 
-const content = document.querySelector('.content');
+// const content = document.querySelector('.content');
 
 // console.log(content.innerHTML);
 // content.innerHTML = '<h2>This is a new H2</h2>';
 
-const people = ['mario', 'luigi', 'yoshi'];
+// const people = ['mario', 'luigi', 'yoshi'];
 
-people.forEach(person => {
-    content.innerHTML += `<p>${person}</p>`
-})
+// people.forEach(person => {
+//     content.innerHTML += `<p>${person}</p>`
+// })
+
+const link = document.querySelector('a');
+
+console.log(link.getAttribute('href'));
+link.setAttribute('href', 'https://www.thenetninja.co.uk');
+link.innerText='The Net Ninja Website';
+
+const mssg = document.querySelector('p');
+console.log(mssg.getAttribute('class'));
+mssg.setAttribute('class', 'success');
+mssg.setAttribute('style', 'color: green;');
