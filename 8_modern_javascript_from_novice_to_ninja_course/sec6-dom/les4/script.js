7f8ecd0f18f7f6b9b564f6fd86eefd4e2263b004
AndriiChemerys <andriichemerys@gmail.com>
2020-11-08 16:24:36 +0100

    Creating & Removing Elements

--- 8_modern_javascript_from_novice_to_ninja_course/sec6-dom/les4/script.js
+++ 8_modern_javascript_from_novice_to_ninja_course/sec6-dom/les4/script.js
@@ -1,18 +1,22 @@
+const ul = document.querySelector('ul');
+// ul.remove();
+
 const button = document.querySelector('button');
 
 button.addEventListener('click', ()=>{
-    console.log('you clicked me')
+    // ul.innerHTML+='<li>something new</li>'
+    const li = document.createElement('li');
+    li.textContent = 'something new to do';
+    // ul.append(li);
+    ul.prepend(li);
 });
 
 const items = document.querySelectorAll('li');
 
 items.forEach(item=>{
     item.addEventListener('click', (e) => {
-        // console.log('item clicked');
-        // console.log(e);
-        console.log(e.target);
-        // console.log(item);
-        e.target.style.textDecoration = 'line-through';
+        // e.target.style.textDecoration = 'line-through';
+        e.target.remove();
     });
 });
 
