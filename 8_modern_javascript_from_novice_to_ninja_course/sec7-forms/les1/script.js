5bb205de80dd580d9f9e0ba676adc87b575fae02
AndriiChemerys <andriichemerys@gmail.com>
2020-11-20 16:19:24 +0100

    Basic Form Validation

--- 8_modern_javascript_from_novice_to_ninja_course/sec7-forms/les1/script.js
+++ 8_modern_javascript_from_novice_to_ninja_course/sec7-forms/les1/script.js
@@ -1,16 +1,28 @@
 const form = document.querySelector('.signup-form');
 //const username = document.querySelector('#username');
+const feedback = document.querySelector('.feedback');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    // console.log(username.value);
+
+    // validation
+    const username = form.username.value;
+    const usernamePattern = /^[a-zA-Z]{6,12}$/;
+
+    if(usernamePattern.test(username)){
+        // feedback good info
+        feedback.textContent = "that username is valid!";
+    } else {
+        // feedback help info
+        feedback.textContent = "username must contain letters only & be between 6 & 12 characters long";
+    }
 
     console.log(form.username.value);
 });
 
 // testing RegEx
-const username = 'shaun';
-const pattern = /[a-z]{6,10}/;
+// const username = 'shaun';
+// const pattern = /^[a-z]{6,}$/;
 
 // let result = pattern.test(username);
 // console.log(result);
@@ -21,6 +33,6 @@ const pattern = /[a-z]{6,10}/;
 //     console.log('regex test failed');
 // }
 
-let result = username.search(pattern);
-console.log(result);
+// let result = username.search(pattern);
+// console.log(result); 
 
