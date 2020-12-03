5be48f07f1540755b5269fb534f914a558242c15
AndriiChemerys <andriichemerys@gmail.com>
2020-12-03 18:53:58 +0100

    Foreach Method & Callbacks

--- 8_modern_javascript_from_novice_to_ninja_course/sec7-forms/les1/script.js
+++ 8_modern_javascript_from_novice_to_ninja_course/sec7-forms/les1/script.js
@@ -39,10 +39,12 @@ form.addEventListener('submit', e => {
 // live feedback
 
 form.username.addEventListener('keyup', e=>{
-    console.log(e.target.value, form.username.value);
-    if(userbanePatter.test(e.target.value)){
-        console.log('passed');
+    // console.log(e.target.value, form.username.value);
+    if(usernamePattern.test(e.target.value)){
+        // console.log('passed');
+        form.username.setAttribute('class', 'success');
     } else {
-        console.log('failed');
+        // console.log('failed');
+        form.username.setAttribute('class', 'error');
     }
 });
