917a6b0c5ab72ff0d5087c0a473959e9b201f220
AndriiChemerys <andriichemerys@gmail.com>
2020-12-29 17:10:44 +0100

    Class Methods & Method Chaining

--- 8_modern_javascript_from_novice_to_ninja_course/sec15-oop/les2/app.js
+++ 8_modern_javascript_from_novice_to_ninja_course/sec15-oop/les2/app.js
@@ -3,6 +3,20 @@ class User {
         // set up properties
         this.username = username;
         this.email = email;
+        this.score = 0;
+    }
+    login(){
+        console.log(`${this.username} just logged in`);
+        return this;
+    }
+    logout(){
+        console.log(`${this.username} just logged out`);
+        return this;
+    }
+    incScore(){
+        this.score += 1;
+        console.log(`${this.username} has score ${this.score}`);
+        return this;
     }
 }
 
@@ -10,6 +24,14 @@ const userOne = new User('Mario', 'mario@mail');
 const userTwo = new User('Luigi', 'luigi@mail');
 
 console.log(userOne, userTwo);
+// userOne.login();
+// userOne.logout();
+// userOne.incScore();
+// userOne.incScore();
+
+userOne.login().incScore().incScore().logout();
+
+// userOne.login().logout();
 
 // the 'new' keyword
 // 1 - it creates a new empty object{}
