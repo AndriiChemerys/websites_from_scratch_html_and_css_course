13a97372ab9ab8a4fb967bed0ca384e335f384db
AndriiChemerys <andriichemerys@gmail.com>
2021-01-25 19:00:47 +0100

    Updating the Room & Username

--- 8_modern_javascript_from_novice_to_ninja_course/sec17-project-chatroom/chatroom_project/scripts/chat.js
+++ 8_modern_javascript_from_novice_to_ninja_course/sec17-project-chatroom/chatroom_project/scripts/chat.js
@@ -4,12 +4,13 @@
 // updating the room
 
 class Chatroom {
-    constructor(room, username){
+    constructor(room, username) {
         this.room = room;
         this.username = username;
         this.chats = db.collection('chats');
+        this.unsub;
     }
-    async addChat(message){
+    async addChat(message) {
         // format a chat object
         const now = new Date();
         const chat = {
@@ -23,26 +24,45 @@ class Chatroom {
         return response;
     }
 
-    getChats(){
-        this.chats
-        .where('room', '==', this.room)
-        .onSnapshot(snapshot => {
-            snapshot.docChanges().forEach(change => {
-                if(change.type === 'added'){
-                    // update the ui
-                    callback(change.doc.data())
-                }
-            })
-        });
+    getChats(callback) {
+        this.unsub = this.chats
+            .where('room', '==', this.room)
+            .orderBy('created_at')
+            .onSnapshot(snapshot => {
+                snapshot.docChanges().forEach(change => {
+                    if (change.type === 'added') {
+                        // update the ui
+                        callback(change.doc.data())
+                    }
+                })
+            });
+    }
+    updateName(username) {
+        this.username = username;
+    }
+    updateRoom(room) {
+        this.room = room;
+        console.log('room updated');
+        if (this.unsub) {
+            this.unsub();
+        }
     }
 }
 
 const chatroom = new Chatroom('gaming', 'shaun');
 
-chatroom.getChats((data)=>{
+chatroom.getChats((data) => {
     console.log(data);
 })
 
-chatroom.addChat('Hello everyone')
-    .then(() => console.log('chat added'))
-    .catch(err => console.log(err));
+setTimeout(() => {
+    chatroom.updateRoom('gaming');
+    chatroom.updateName('yoshi');
+    chatroom.getChats((data) => {
+        console.log(data);
+    });
+    chatroom.addChat('Hello everyone')
+        // .then(() => console.log('chat added'))
+        // .catch(err => console.log(err));
+}, 3000);
+
