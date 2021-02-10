269a6f1d10beff64239147300a7e8d5f499ccc13
AndriiChemerys <andriichemerys@gmail.com>
2021-02-10 21:44:32 +0100

    Default Exports{ git commit -m Default

---
+++ 8_modern_javascript_from_novice_to_ninja_course/sec19-babel_webpack/les1/src/data.js
@@ -0,0 +1,10 @@
+const users = [
+    {name: 'mario', premium: true},
+    {name: 'luigi', premium: false},
+    {name: 'yoshi', premium: true},
+    {name: 'toad', premium: true},
+    {name: 'peach', premium: true},
+];
+
+export default users;
+
--- 8_modern_javascript_from_novice_to_ninja_course/sec19-babel_webpack/les1/src/index.js
+++ 8_modern_javascript_from_novice_to_ninja_course/sec19-babel_webpack/les1/src/index.js
@@ -1,5 +1,8 @@
 import './dom';
 import { styleBody, addTitle, contact} from './dom';
+import users from './data';
+
+console.log(users);
 
 console.log('index file');
 addTitle('Test');
