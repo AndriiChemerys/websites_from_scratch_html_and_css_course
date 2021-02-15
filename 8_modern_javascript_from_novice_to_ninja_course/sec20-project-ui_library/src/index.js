8e05b3ec80e2a2a31e48ba34f71bb6dc07fe67cf
AndriiChemerys <andriichemerys@gmail.com>
2021-02-15 21:52:11 +0100

    Tooltips

--- 8_modern_javascript_from_novice_to_ninja_course/sec20-project-ui_library/src/index.js
+++ 8_modern_javascript_from_novice_to_ninja_course/sec20-project-ui_library/src/index.js
@@ -1,1 +1,8 @@
+import './test.css';
+import Tooltip from './ui/tooltip';
 
+// create a tooltip
+const tooltip = new Tooltip(document.querySelector('.tooltip'));
+tooltip.init();
+
+console.log("test A");
