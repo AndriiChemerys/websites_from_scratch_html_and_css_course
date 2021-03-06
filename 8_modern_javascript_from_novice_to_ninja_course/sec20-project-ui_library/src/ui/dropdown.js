812df5aba1fc0e2fe403eab3504bdf97b91503f2
AndriiChemerys <andriichemerys@gmail.com>
2021-03-06 16:14:56 +0100

    Dropdowns

---
+++ 8_modern_javascript_from_novice_to_ninja_course/sec20-project-ui_library/src/ui/dropdown.js
@@ -0,0 +1,7 @@
+class Dropdown {
+    constructor(container){
+        this.container = container;
+        this.trigger = container.querySelector('.trigger');
+        this.content = content.querySelector('.content');
+    }
+}
