b330a4d406f62dda7e15a1259cd0276439f84476
AndriiChemerys <andriichemerys@gmail.com>
2021-03-10 21:11:19 +0100

    Tabbed Content

--- 8_modern_javascript_from_novice_to_ninja_course/sec20-project-ui_library/src/ui/tabs.js
+++ 8_modern_javascript_from_novice_to_ninja_course/sec20-project-ui_library/src/ui/tabs.js
@@ -14,5 +14,14 @@ class Tabs {
     toggleTabs(e){
         // remove current active classes
         this.tabs.forEach(tab=>tab.classList.remove('active'));
+        // add new active class to clicked tab
+        e.target.classList.add('active');
+    }
+    toggleContent(e){
+        // remove current active classes from content
+        this.container.querySelectorAll('.content').forEach(item=>{
+            item.classList.remove('active');
+        });
+        // add new active class to content
     }
 }
