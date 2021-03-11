bf456980dea79b2e117f71c07e56b47ae70fda8a
AndriiChemerys <andriichemerys@gmail.com>
2021-03-11 18:55:22 +0100

    Tabbed Content

--- 8_modern_javascript_from_novice_to_ninja_course/sec20-project-ui_library/src/index.js
+++ 8_modern_javascript_from_novice_to_ninja_course/sec20-project-ui_library/src/index.js
@@ -1,6 +1,7 @@
 import './test.css';
 import Tooltip from './ui/tooltip';
 import Dropdown from './ninja-ui/dropdown';
+import Tabs from '.ninja-ui/tabs';
 
 // create a tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
@@ -14,4 +15,9 @@ const dropdowns = document.querySelectorAll('.dropdown');
 dropdowns.forEach(dropdown => {
     const instance = new Dropdown(dropdown);
     instance.init();
-})
+});
+
+//create tabs
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
+
