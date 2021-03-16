f49049f2fc9a4c6b1eec7482dde378fdf6df9f48
AndriiChemerys <andriichemerys@gmail.com>
2021-03-16 21:54:56 +0100

    Snackbars

--- 8_modern_javascript_from_novice_to_ninja_course/sec20-project-ui_library/src/index.js
+++ 8_modern_javascript_from_novice_to_ninja_course/sec20-project-ui_library/src/index.js
@@ -2,6 +2,7 @@ import './test.css';
 import Tooltip from './ui/tooltip';
 import Dropdown from './ninja-ui/dropdown';
 import Tabs from '.ninja-ui/tabs';
+import Snackbar from './ninja-ui/snackbar';
 
 // create a tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
@@ -21,3 +22,6 @@ dropdowns.forEach(dropdown => {
 const tabs = new Tabs(document.querySelector('.tabs'));
 tabs.init();
 
+// create snackbar
+const snackbar = new Snackbar();
+snackbar.init();
