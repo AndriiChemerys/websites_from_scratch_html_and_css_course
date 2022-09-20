5d0e53f382c7a7e7c7baa5f7cb259c47c4e0f74d
AndriiChemerys <andriichemerys@gmail.com>
2022-09-20 18:21:12 +0200

    48. Monsters Rolodex - SearchBox Component~

--- 10_Complete_React_Developer_in_2022/section_3/project_1/app/src/App.js
+++ 10_Complete_React_Developer_in_2022/section_3/project_1/app/src/App.js
@@ -1,5 +1,7 @@
 import { Component } from "react";
 
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 import "./App.css";
 
 class App extends Component {
@@ -16,14 +18,9 @@ class App extends Component {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
-        this.setState(
-          () => {
-            return { monsters: users };
-          },
-          () => {
-            console.log(this.state);
-          }
-        )
+        this.setState(() => {
+          return { monsters: users };
+        })
       );
   }
 
@@ -46,19 +43,12 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input
+        <SearchBox
           className="search-box"
-          type="search"
-          placeholder="search monsters"
-          onChange={onSearchChange}
+          onChangeHandler={onSearchChange}
+          placeholder={"search monsters"}
         />
-        {filteredMonsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
-        })}
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
